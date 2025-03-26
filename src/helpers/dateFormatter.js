0fae46a459d65dbf7bd14063565e67bcfb097677
O. Douglas Idumwonyi <id.doglax@gmail.com>
2025-03-26 01:31:33 +0100

    Update: Added Pdf viewer for IWD Brochure

---
+++ src/helpers/dateFormatter.js
@@ -0,0 +1,5 @@
+export const shortDateFormatter = new Intl.DateTimeFormat('en-US', {
+  day: 'numeric',
+  month: 'short',
+  year: 'numeric',
+})
---
+++ src/helpers/generateImageUrl.js
@@ -0,0 +1,9 @@
+import ImageUrlBuilder from '@sanity/image-url'
+
+import { client } from './sanityClient'
+
+const imgUrlBuilder = ImageUrlBuilder(client) // Get a pre-configured url-builder from my sanity client
+
+export const imgUrl = imgSrc => {
+  return imgUrlBuilder.image(imgSrc)
+}
