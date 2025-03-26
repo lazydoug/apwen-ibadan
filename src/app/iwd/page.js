b064d7a01cb7da415890a570e9b172982dd81816
O. Douglas Idumwonyi <id.designrr@gmail.com>
2025-03-26 02:05:49 +0100

    Fix: Changed git user email

--- src/app/iwd/page.js
+++ src/app/iwd/page.js
@@ -37,7 +37,7 @@ const IWDBrochure = () => {
   }, [isClient])
 
   if (!isClient) {
-    return null // Prevents rendering on the server.
+    return null // Prevents rendering on the server
   }
 
   return <div ref={containerRef} style={{ height: '100vh' }} />
