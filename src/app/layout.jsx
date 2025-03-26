729cecc6cc7bade34f0fee9b629f421e8a140dad
O. Douglas Idumwonyi <id.doglax@gmail.com>
2025-03-26 01:57:35 +0100

    Update: Added SpeedInsight Component

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,3 +1,5 @@
+import { SpeedInsights } from '@vercel/speed-insights/next'
+
 import { Plus_Jakarta_Sans } from 'next/font/google'
 import './globals.css'
 
@@ -16,6 +18,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body className={`${plusJakartaSans.className} bg-purple-98 min-w-80`}>
+        <SpeedInsights />
         <header>
           <NavBar />
         </header>
